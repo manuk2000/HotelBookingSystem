780b37543f9848bc3e49ae337b2ec1fee4d723b7
Manuk <123746895+manuk2000@users.noreply.github.com>
2023-09-28 18:51:52 +0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,56 +1,4 @@
-package org.example;//package org.example;
-//
-//import org.example.consumer.ConsumerType;
-//import org.example.consumer.Customer;
-//import org.example.consumer.ICustomer;
-//import org.example.history.StartEndDate;
-//import org.example.room.Room;
-//import org.example.room.TypeRoom;
-//import org.example.roomProperty.IRoomProperty;
-//import org.example.roomProperty.RoomProperty;
-//
-//import java.io.*;
-//
-//import java.time.LocalDate;
-//import java.util.Arrays;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//
-//        // Serialize Hotel object to a file
-//        Hotel h = new Hotel();
-//
-//
-//        String filePath = "hotel.ser";
-//        h.serializeHotel(filePath);
-//
-//        // Deserialize Hotel object from a file
-//        Hotel deserializedHotel = h.deserializeHotel(filePath);
-//        if (deserializedHotel != null) {
-//            // ... perform operations on the deserializedHotel object ...
-//
-//            IRoomProperty prop1 = new RoomProperty(2, "prop");
-//            IRoomProperty prop2 = new RoomProperty(2, "prop");
-//            Room room = new Room(Arrays.asList(prop1, prop2), 12, TypeRoom.SINGLE_ROOM);
-//            h.addRoom(room);
-//            ICustomer consumer = new Customer("name", "email", ConsumerType.SIMPLE);
-//            h.addCunsomer(consumer);
-//
-//
-//            LocalDate startDate1 = LocalDate.of(2023, 7, 15);
-//            LocalDate endDate1 = LocalDate.of(2023, 7, 17);
-//
-//            LocalDate startDate2 = LocalDate.of(2023, 7, 16);
-//            LocalDate endDate2 = LocalDate.of(2023, 7, 18);
-//            StartEndDate startEndDate1 = StartEndDate.createDataTime(startDate1, endDate1);
-//            StartEndDate startEndDate2 = StartEndDate.createDataTime(startDate2, endDate2);
-//
-//            h.bookRoom(consumer, TypeRoom.SINGLE_ROOM, startEndDate1);
-//        }
-//    }
-//}
-
-
+package org.example;
 import org.example.Hotel;
 import org.example.consumer.Customer;
 import org.example.consumer.ICustomer;
@@ -79,6 +27,9 @@ public class Main {
 
 // Deserialize Hotel object from a file
         hotel = Hotel.deserializeHotel(filePath);
+        if(hotel == null){
+            hotel = new Hotel();
+        }
 
         Scanner scanner = new Scanner(System.in);
 
